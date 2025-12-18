f3a852d5544172c8921875b30dcf4cbc78266491
Dhyan Suresh Agni <dhyanagni2001@gmail.com>
2025-12-18 13:43:54 -0800

    Time: 0 ms (100%), Space: 9.9 MB (10.97%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()){
+        return false;
+        }
+        vector<int> freq(26,0);
+
+        for(char c : s){
+            freq[c - 'a']++;
+        }
+
+        for(char c : t){
+            freq[c - 'a']--;
+        }
+
+    return all_of(freq.begin(), freq.end(), [](int x){return x == 0;});
+    
+
+    
+    }
+};
