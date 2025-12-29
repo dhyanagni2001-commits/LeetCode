4344ca3aaa7761a2cc3a8e6646d8126ce8140ce7
Dhyan Suresh Agni <dhyanagni2001@gmail.com>
2025-12-28 22:24:56 -0800

    Time: 2770 ms (5.06%), Space: 34.3 MB (87.76%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,25 +1,42 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
-        mp[0] = 1;        // one way to have prefix sum 0
+    //     unordered_map<int,int> mp;
+    //     mp[0] = 1;        // one way to have prefix sum 0
 
-        int prefix = 0;
-        int count = 0;
+    //     int prefix = 0;
+    //     int count = 0;
 
-        for(int x : nums){
+    //     for(int x : nums){
 
-            prefix += x;
+    //         prefix += x;
 
-            // check if there exists (prefix - k)
-            if(mp.find(prefix - k) != mp.end()){
-                count += mp[prefix - k];
+    //         // check if there exists (prefix - k)
+    //         if(mp.find(prefix - k) != mp.end()){
+    //             count += mp[prefix - k];
+    //         }
+
+    //         // store current prefix
+    //         mp[prefix]++;
+    //     }
+
+    //     return count;
+    // }
+    
+    long long count=0;
+    long long i;
+    long long j;
+    for( i=0;i<nums.size();i++){
+         long long sum=0;
+        for(j=i;j<nums.size();j++){
+            sum=sum+nums[j];
+            if(sum==k){
+                count++;
+                
             }
-
-            // store current prefix
-            mp[prefix]++;
         }
-
-        return count;
     }
+    return count;
+    }
+
 };
