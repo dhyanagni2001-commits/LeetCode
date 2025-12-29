5827fc02e298fe6c02d0cd756ff3112468623c2b
Dhyan Suresh Agni <dhyanagni2001@gmail.com>
2025-12-28 23:27:58 -0800

    Time: 1 ms (35.72%), Space: 97.5 MB (24.94%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,19 +3,11 @@ public:
     int maxProfit(vector<int>& prices) {
         int minPrice = INT_MAX;
         int maxProfit = 0;
-
-        for(int price : prices){
-
-            // 1) keep track of minimum price so far
-            minPrice = min(minPrice, price);
-
-            // 2) profit if sold today
-            int profit = price - minPrice;
-
-            // 3) update answer
+        for(int x: prices){
+            minPrice = min(minPrice, x);
+            int profit = x - minPrice;
             maxProfit = max(maxProfit, profit);
         }
-
         return maxProfit;
     }
 };
