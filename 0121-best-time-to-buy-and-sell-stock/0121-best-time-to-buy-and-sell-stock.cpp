5537683558c69b564cb925d9f8b8397310c5c09f
Dhyan Suresh Agni <dhyanagni2001@gmail.com>
2026-01-01 11:55:04 -0800

    Time: 5 ms (8.73%), Space: 97.4 MB (24.83%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
         int profit=0;
         int minnn=prices[0];
         for(int i=0;i<prices.size();i++){
-            min=min(minnn,prices[i]);
+            minnn=min(minnn,prices[i]);
             profit=max(profit, prices[i]-minnn);
         }
         return profit;
