7205eaf90b26b3b9ceb8dda723710ccef1df148a
Dhyan Suresh Agni <dhyanagni2001@gmail.com>
2026-01-06 16:12:42 -0800

    Time: 0 ms (100%), Space: 23.5 MB (95.45%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -12,33 +12,79 @@ public:
         //     }
         // }
         // return ans;
-        vector<int> ans;
-        int n = nums.size();
+        // vector<int> ans;
+        // int n = nums.size();
 
-        for(int i = 0; i < n; i++){
+        // for(int i = 0; i < n; i++){
 
-            int count = 0;
+        //     int count = 0;
 
-            // avoid counting an element already added
-            if(ans.size() >= 1 && nums[i] == ans[0]) continue;
-            if(ans.size() == 2 && nums[i] == ans[1]) continue;
+        //     // avoid counting an element already added
+        //     if(ans.size() >= 1 && nums[i] == ans[0]) continue;
+        //     if(ans.size() == 2 && nums[i] == ans[1]) continue;
 
-            // count frequency of nums[i]
-            for(int j = 0; j < n; j++){
-                if(nums[i] == nums[j]){
-                    count++;
+        //     // count frequency of nums[i]
+        //     for(int j = 0; j < n; j++){
+        //         if(nums[i] == nums[j]){
+        //             count++;
+        //         }
+        //     }
+
+        //     // check majority condition
+        //     if(count > n/3){
+        //         ans.push_back(nums[i]);
+        //     }
+
+        //     // maximum 2 possible answers
+        //     if(ans.size() == 2) break;
+        // }
+
+        // return ans;
+        int count1=0;
+        int count2=0;
+        int el1=INT_MIN;
+        int el2=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            if(count1==0&& nums[i]!=el2){
+                count1=1;
+                el1=nums[i];
+            }
+            else if(count2==0&& nums[i]!=el1){
+                    count2=1;
+                    el2=nums[i];
                 }
+            else if(el1==nums[i]){
+                count1++;
             }
-
-            // check majority condition
-            if(count > n/3){
-                ans.push_back(nums[i]);
+            else if(el2==nums[i]){
+                count2++;
             }
+            else{
+                count1--;
+                count2--;
+            }
+               
 
-            // maximum 2 possible answers
-            if(ans.size() == 2) break;
+        }
+    int cou=0;
+    int coun=0;
+    vector<int> ans;
+    for(int i=0;i<nums.size();i++){
+        if(el1==nums[i]){
+            cou++;
+        }
+        else if(el2==nums[i]){
+            coun++;
         }
 
-        return ans;
+    }
+    int min = (int)(nums.size()/3);
+    if(cou > min) {
+        ans.push_back(el1);
+    }
+    if(coun > min) {
+        ans.push_back(el2);
+    }
+    return ans;
     }
 };
