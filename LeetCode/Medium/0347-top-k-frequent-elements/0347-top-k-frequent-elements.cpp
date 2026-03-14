9af01fdef2c34e2d04bcff4412e708ebd9b361e3
Dhyan Suresh Agni <dhyanagni2001@gmail.com>
2026-03-13 20:26:42 -0700

    Time: 0 ms (100%), Space: 20 MB (10.62%) - LeetHub

--- LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -19,18 +19,38 @@ public:
     //         res.push_back(v[i].second);
     //     }
     // return res;
-    unordered_map<int,int> mpp;
-    for(int a: nums){
-        mpp[a]++;
+    // unordered_map<int,int> mpp;
+    // for(int a: nums){
+    //     mpp[a]++;
+    // }
+    // vector<pair<int,int>> v;
+    // for(auto b: mpp){
+    //     v.push_back({b.second,b.first});
+    // }
+    // sort(v.rbegin(),v.rend());
+    // vector <int> ans;
+    // for(int i = 0; i<k; i++){
+    //     ans.push_back(v[i].second);
+    // }
+    // return ans;
+    unordered_map<int, int> freq;
+    for(int a : nums){
+        freq[a]++;
     }
-    vector<pair<int,int>> v;
-    for(auto b: mpp){
-        v.push_back({b.second,b.first});
+    vector<vector<int>> bucket(nums.size()+1);
+    for(auto &p: freq){
+        int frequency = p.second;
+        int value = p.first;
+        bucket[frequency].push_back(value);
     }
-    sort(v.rbegin(),v.rend());
-    vector <int> ans;
-    for(int i = 0; i<k; i++){
-        ans.push_back(v[i].second);
+    vector<int> ans;
+    for(int i = nums.size(); i>=0 && ans.size() < k; i--){
+        for(int num : bucket[i]){
+            ans.push_back(num);
+            if(ans.size()==k){
+                break;
+            }
+        }
     }
     return ans;
     }
