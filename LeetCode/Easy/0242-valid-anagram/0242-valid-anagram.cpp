04a231bb5288bc91d62c6cc70deddda491fd9a31
Dhyan Suresh Agni <dhyanagni2001@gmail.com>
2026-03-13 17:30:44 -0700

    Time: 0 ms (100%), Space: 9.6 MB (73.86%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.cpp
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.cpp
@@ -16,18 +16,29 @@ public:
 
     // return all_of(freq.begin(), freq.end(), [](int x){return x == 0;});
     // return all_of(freq.begin(), freq.end(), [](int x) {return x==0});
+    // if(s.size()!=t.size()){
+    //     return false;
+    // }
+    // vector<int> freq(26,0);
+
+    // for(char c:s ){
+    //     freq[c - 'a']++;
+    // }
+    // for(char c:t ){
+    //     freq[c - 'a']--;
+    // }
+    
+    // return all_of(freq.begin(),freq.end(),[](int x){return x==0;});
     if(s.size()!=t.size()){
         return false;
     }
-    vector<int> freq(26,0);
-
-    for(char c:s ){
-        freq[c - 'a']++;
+    vector <int> freq(26,0);
+    for(char ch : s){
+        freq[ch - 'a']++;
     }
-    for(char c:t ){
-        freq[c - 'a']--;
+    for(char ch : t){
+        freq[ch - 'a']--;
     }
     return all_of(freq.begin(),freq.end(),[](int x){return x==0;});
-    
     }
 };
