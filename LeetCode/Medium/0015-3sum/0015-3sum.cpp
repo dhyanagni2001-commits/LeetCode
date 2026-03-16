390b22eb78522d31b9df1a26b126b231aff90837
Dhyan Suresh Agni <dhyanagni2001@gmail.com>
2026-03-16 11:21:33 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0015-3sum/0015-3sum.cpp
+++ LeetCode/Medium/0015-3sum/0015-3sum.cpp
@@ -34,54 +34,54 @@ public:
     // }
     //    return vector<vector<int>>(ans.begin(), ans.end());
 
+    // vector<vector<int>> ans;
+    // sort(nums.begin(),nums.end());
+    // for(int i=0;i<nums.size();i++){
+    //     if(i>0 && nums[i]==nums[i-1]) continue;
+    //     int j=i+1;
+    //     int k=nums.size()-1;
+    //     while(j<k){
+    //         int sum=nums[i]+nums[j]+nums[k];
+    //         if(sum<0){
+    //             j++;
+    //         }else if(sum > 0){
+    //             k--;
+    //         }else{
+    //             vector<int> temp ={nums[i],nums[j],nums[k]};
+    //             ans.push_back(temp);
+    //             j++;
+    //             k--;
+    //             while(j<k && nums[j]==nums[j-1]) j++;
+    //             while(j<k && nums[k]==nums[k+1]) k--;
+    //         }
+    //     }
+    // }
+    return ans;
+    sort(nums.begin(), nums.end());
     vector<vector<int>> ans;
-    sort(nums.begin(),nums.end());
-    for(int i=0;i<nums.size();i++){
+
+    for(int i =0 ;i<nums.size();i++){
         if(i>0 && nums[i]==nums[i-1]) continue;
-        int j=i+1;
-        int k=nums.size()-1;
+        int j = i+1;
+        int k = nums.size()-1;
         while(j<k){
-            int sum=nums[i]+nums[j]+nums[k];
-            if(sum<0){
-                j++;
-            }else if(sum > 0){
+            int sum = nums[i]+nums[j]+nums[k];
+            if(sum>0){
                 k--;
+            }else if(sum<0){
+                j++;
             }else{
-                vector<int> temp ={nums[i],nums[j],nums[k]};
-                ans.push_back(temp);
+                vector<int> ans1 = {nums[i],nums[j],nums[k]};
+                ans.push_back(ans1);
                 j++;
                 k--;
-                while(j<k && nums[j]==nums[j-1]) j++;
-                while(j<k && nums[k]==nums[k+1]) k--;
+                while(j<k && nums[j]==nums[j-1])j++;
+                while(j<k && nums[k]==nums[k-1])k--;
             }
         }
-    }
-    return ans;
-//     sort(nums.begin(), nums.end());
-//     vector<vector<int>> ans;
-
-//     for(int i =0 ;i<nums.size();i++){
-//         if(i>0 && nums[i]==nums[i-1]) continue;
-//         int j = i+1;
-//         int k = nums.size()-1;
-//         while(j < k){
-//             int sum = nums[i]+nums[j]+nums[k];
-//             if(sum>0){
-//                 k--;
-//             }else if(sum<0){
-//                 j++;
-//             }else{
-//                 vector<int> ans1 = {nums[i],nums[j],nums[k]};
-//                 ans.push_back(ans1);
-//                 j++;
-//                 k--;
-//                 while(j<k && nums[j]==nums[j-1])j++;
-//                 while(j<k && nums[k]==nums[k-1])k--;
-//             }
-//         }
        
-//     }
-//      return ans;
+    }
+     return ans;
     }
 
 };
