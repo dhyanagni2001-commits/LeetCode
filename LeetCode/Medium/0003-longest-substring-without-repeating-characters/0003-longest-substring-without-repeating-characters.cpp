6f561175cef1c4679396417ee47cffbd15c12842
Dhyan Suresh Agni <dhyanagni2001@gmail.com>
2026-04-01 17:43:04 -0700

    Time: 23 ms (17.45%), Space: 14.5 MB (13.19%) - LeetHub

--- LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -15,18 +15,34 @@ public:
         // }
         // return ans;
 
+        // unordered_set<int> c;
+        // int p = 0;
+        // int ans = 0;
+
+        // for(int i=0;i<s.size();i++){
+        //     while(c.find(s[i])!= c.end()){
+        //         c.erase(s[p]);
+        //         p++;
+        //     }
+        //     c.insert(s[i]);
+        //     ans = max(ans, i-p+1);
+
+        // }
+
+        // return ans;
+
         unordered_set<int> c;
+
         int p = 0;
         int ans = 0;
 
-        for(int i=0;i<s.size();i++){
+        for(int i =0;i<s.size();i++){
             while(c.find(s[i])!= c.end()){
                 c.erase(s[p]);
                 p++;
             }
             c.insert(s[i]);
-            ans = max(ans, i-p+1);
-
+            ans =max(ans, i-p+1);
         }
 
         return ans;
