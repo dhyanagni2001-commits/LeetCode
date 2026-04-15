fab505ca1ed63c843e1d170c656a392e4412179f
Dhyan Suresh Agni <dhyanagni2001@gmail.com>
2026-04-15 13:31:28 -0700

    Time: 11 ms (91.15%), Space: 26.2 MB (38.32%) - LeetHub

--- LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.cpp
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,34 +1,18 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-    //     unordered_map<string,vector<string>> mp;
-    //     for(string s : strs){
-    //         string key = s;
-
-    //     sort(key.begin(),key.end());
-    //     mp[key].push_back(s);
-    //     }
-
-    //     vector<vector<string>> ans;
-
-    //     for(auto &it: mp){
-    //         ans.push_back(it.second);
-    //     }
-
-    //     return ans;
-        unordered_map<string,vector<string>> mp;
-        for(string s : strs){
-            string key = s;
-
-            sort(key.begin(),key.end());
-            mp[key].push_back(s);
+        unordered_map<string, vector<string>> mp;
+        for(auto p : strs){
+            string k = p;
+            sort(k.begin(), k.end());
+            mp[k].push_back(p);
         }
 
-        vector<vector<string>> ans;
-
-        for(auto it:mp){
-            ans.push_back(it.second);
+         vector<vector<string>> ans;
+        for(auto p : mp){
+            ans.push_back(p.second);
         }
+
         return ans;
-     }
+    }
 };
