a794555f2488bf625d99379ded96da221652da9b
Dhyan Suresh Agni <dhyanagni2001@gmail.com>
2026-04-15 16:26:14 -0700

    Time: 3 ms (49.99%), Space: 19.9 MB (10.63%) - LeetHub

--- LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,81 +1,29 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-    //     unordered_map <int,int> mpp;
-    //     for(int a: nums){
-    //         mpp[a]++;
-    //     }
-
-    //     vector<pair<int,int>> v;
-    //     for(auto p: mpp){
-    //         v.push_back({p.second,p.first});
-    //     }
-
-    //     sort(v.rbegin(),v.rend()); 
-    //    //sorting by frequency 
-
-    //     vector<int> res;
-    //     for(int i=0;i<k;++i){
-    //         res.push_back(v[i].second);
-    //     }
-    // return res;
-    // unordered_map<int,int> mpp;
-    // for(int a: nums){
-    //     mpp[a]++;
-    // }
-    // vector<pair<int,int>> v;
-    // for(auto b: mpp){
-    //     v.push_back({b.second,b.first});
-    // }
-    // sort(v.rbegin(),v.rend());
-    // vector <int> ans;
-    // for(int i = 0; i<k; i++){
-    //     ans.push_back(v[i].second);
-    // }
-    // return ans;
-    // unordered_map<int, int> freq;
-    // for(int a : nums){
-    //     freq[a]++;
-    // }
-    // vector<vector<int>> bucket(nums.size()+1);
-    // for(auto &p: freq){
-    //     int frequency = p.second;
-    //     int value = p.first;
-    //     bucket[frequency].push_back(value);
-    // }
-    // vector<int> ans;
-    // for(int i = nums.size(); i>=0 && ans.size() < k; i--){
-    //     for(int num : bucket[i]){
-    //         ans.push_back(num);
-    //         if(ans.size()==k){
-    //             break;
-    //         }
-    //     }
-    // }
-    // return ans;
-
-    unordered_map<int,int> mpp;
-    for(int a : nums){
-        mpp[a]++;
-    }
-
-    vector<vector<int>> bucket(nums.size()+1);
-
-    for(auto &p : mpp){
-        int frequency = p.second;
-        int value = p.first;
-        bucket[frequency].push_back(value);
-    }
-
-    vector<int> ans;
-    for(int i = nums.size(); i>0 && ans.size()<k; i--){
-        for(auto p : bucket[i]){
-            ans.push_back(p);
-            if(ans.size()==k){
-            break;
-            }
+        unordered_map<int, int> mpp;
+        for(auto p : nums){
+            mpp[p]++;
         }
+        vector<vector<int>> bucket(nums.size()+1);
+
+        for(auto p : mpp){
+            int freq = p.second;
+            int value = p.first;
+            bucket[freq].push_back(value);
+        }
+
+        vector<int> ans;
+        for(int i = nums.size(); i >0 && ans.size()<k; i--){
+            for(auto p : bucket[i]){
+                ans.push_back(p);
+                if(ans.size()==k){
+                    break;
+                }
+            }
+
+
+        }
+        return ans;
     }
-    return ans;
-     }
 };
