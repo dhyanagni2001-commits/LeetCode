98a4407cf6cd0e9e8fdfddaf8c4944820f0669e1
Dhyan Suresh Agni <dhyanagni2001@gmail.com>
2026-05-19 15:01:01 -0700

    Time: 3 ms (66.5%), Space: 14.8 MB (54.42%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> mp;
-        for (int i =0;i<nums.size();i++){
+        for(int i = 0;i<nums.size();i++){
             int curr = nums[i];
             int diff = target - curr;
+            
             if(mp.find(diff)!=mp.end()){
-                return {i,mp[diff]};
+                return {mp[diff],i};
             }
-            mp[curr] = i;
+            mp[curr]=i;
         }
         return {};
+        
     }
 };
