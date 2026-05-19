9c5e79a0c9320eccab2622f842633eae27d6d77d
Dhyan Suresh Agni <dhyanagni2001@gmail.com>
2026-05-19 10:41:38 -0700

    Time: 64 ms (18.39%), Space: 92.9 MB (12.52%) - LeetHub

--- LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.cpp
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,39 +1,15 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-    //   unordered_set<int> s;
-    //   for(int x : nums){
-    //    if (s.count(x)==1) {
-    //         return true;
-    //     }
-    //     s.insert(x);
-
-    //   }
-    // return false;
-        
-    //   }
-    // unordered_map<int,int> new11;
-    // for(int x : nums){
-    //     new11[x]++;
-    // }
-    // for(auto y : new11){
-    //     if(y.second>1){
-    //         return true;
-    //     }
-    
-        
-    // }
-    // return false;
-
-    unordered_map<int,int> mp;
-    for(auto m:nums){
-        mp[m]++;
-    }
-    for(auto p:mp){
-        if(p.second >1){
-            return true;
+        unordered_map<int,int> mp;
+        for( auto p: nums){
+            mp[p]++;
         }
-    }
-    return false;
+        for(auto a: mp){
+            if(a.second > 1){
+                return true;
+            }
+        }
+        return false;
     }
 };
