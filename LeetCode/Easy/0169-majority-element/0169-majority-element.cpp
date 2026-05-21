7fb17b4f567791ed90aa0906a0a5be6cced6b934
Dhyan Suresh Agni <dhyanagni2001@gmail.com>
2026-05-20 19:03:42 -0700

    Time: 3 ms (28.16%), Space: 28.1 MB (63.33%) - LeetHub

--- LeetCode/Easy/0169-majority-element/0169-majority-element.cpp
+++ LeetCode/Easy/0169-majority-element/0169-majority-element.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-    int count = 0;
-    int s;
-    for(int i =0;i<nums.size();i++){
-        if(count==0){
-           s=nums[i];
-           count++;
-        }else if(s==nums[i]){
+    int n;
+    int count=0;
+    for(int i=0;i<nums.size();i++){
+        
+        if(count == 0){
+            count++;
+            n = nums[i];
+        }else if(n==nums[i]){
             count++;
         }else{
             count--;
         }
+
     }
-    return s;
+    return n;
     }
 };
