636f507864ddc73cf0d51f87e2728d0f1227b4a6
Dhyan Suresh Agni <dhyanagni2001@gmail.com>
2026-05-20 18:23:44 -0700

    Time: 0 ms (100%), Space: 11.5 MB (77.41%) - LeetHub

--- LeetCode/Medium/0075-sort-colors/0075-sort-colors.cpp
+++ LeetCode/Medium/0075-sort-colors/0075-sort-colors.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int colour1 = 0;
-        int colour2 = 0;
-        int colour3 = 0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-                colour1++;
-            }
-            else if(nums[i]==1){
-                colour2++;
-            }else{
-                colour3++;
-            }
+       int count1=0;
+       int count2=0;
+       int count3=0;
+       for(int a: nums){
+        if(a == 0){
+            count1++;
+        }else if(a==1){
+            count2++;
+        }else{
+            count3++;
         }
-        for(int i = 0; i < colour1 ; i++){
-            nums[i] = 0;
-        }
-        for(int i = colour1; i < colour1+colour2 ; i++){
-            nums[i] = 1;
-        }
-         for(int i = colour1+colour2; i < colour1+colour2+colour3 ; i++){
-            nums[i] = 2;
-        }
+       } 
+
+       for(int i=0;i<count1;i++){
+        nums[i]=0;
+       }
+       for(int i=count1;i<count1+count2;i++){
+        nums[i]=1;
+       }
+       for(int i=count1+count2;i<count1+count2+count3;i++){
+        nums[i]=2;
+       }
        
+    
+
     }
 };
